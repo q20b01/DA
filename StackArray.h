0b36ba0d3d3033c1a8a434e5aff975c67f1d3be0
q20b01 <qubitos@o2.pl>
2015-09-21 18:16:48 +0100

    QueueArray initial ver, small changes in StackArray

--- StackArray.h
+++ StackArray.h
@@ -8,17 +8,16 @@ class StackArray
 {
 public:
 	StackArray(unsigned int size = STACK_MIN_SIZE) :
+		m_array(NULL),
 		m_size(size > STACK_MIN_SIZE ? size : STACK_MIN_SIZE),
-		m_sp(0),
-		m_array(NULL)
+		m_sp(0)
 	{
 		m_array = (T*)malloc(m_size * sizeof(T));
 	}
 
 	~StackArray()
 	{
-		if(m_array)
-			free(m_array);
+		if(m_array)	free(m_array);
 	}
 
 	void push(T const& item)
@@ -34,7 +33,7 @@ public:
 
 		item = m_array[--m_sp];
 
-		if(m_sp < (m_size / 4))	resizeArray(m_size / 4);
+		if(m_sp < (m_size / 4))	resizeArray(m_size / 2);
 
 		return true;
 	}
@@ -50,7 +49,7 @@ private:
 		if(size < STACK_MIN_SIZE)	return;
 
 #ifdef _QDEBUG
-		printf("%s:%d %s m_sp: %u | m_size: %u | size: %u\n", __FILE__, __LINE__, __FUNCTION__, m_sp, m_size, size);
+		printf("\n%s:%d %s m_sp: %u | m_size: %u | size: %u\n\n", __FILE__, __LINE__, __FUNCTION__, m_sp, m_size, size);
 #endif
 
 		T* tmp = (T*)realloc(m_array, size * sizeof(T));
@@ -64,11 +63,15 @@ private:
 
 		m_size = size;
 		m_array = tmp;
+#ifdef _QDEBUG
+		printf("\n%s:%d %s m_sp: %u | m_size: %u \n\n", __FILE__, __LINE__, __FUNCTION__, m_sp, m_size);
+#endif
 	}
 
 private:
+	T* m_array;
+
 	unsigned int m_size;
 	unsigned int m_sp;
 
-	T* m_array;
 };
--- test.cpp
+++ test.cpp
@@ -5,12 +5,15 @@
  *      Author: qbit
  */
 
-#include "StackArray.h"
+#include <time.h>
+#include <stdlib.h>
 
-int main()
+#include "StackArray.h"
+#include "QueueArray.h"
+
+void testStack()
 {
 	StackArray<int>	array;
-
 
 	for(unsigned int i = 0; i < 128; ++i)
 	{
@@ -21,14 +24,90 @@ int main()
 	int val;
 	for(unsigned int i = 0; i < 128; ++i)
 	{
-		if(array.pop(val))	printf("val: %d \n", val);
+		if(array.pop(val))	printf("%d ", val);
 	}
+	printf("\n");
 
-	for(unsigned int i = 0; i < 50; ++i)
+	for(unsigned int i = 0; i < 10; ++i)
 	{
 		if(array.pop(val))	printf("val: %d \n", val);
-		else printf("empty\n");
+		else 	printf("empty\n");
 	}
+}
+
+void testQueue()
+{
+	QueueArray<int>	queue;
+
+	for(unsigned int i = 0; i < 128; ++i)
+	{
+		queue.enqueue(i);
+	}
+
+	int val;
+	for(unsigned int i = 0; i < 128; ++i)
+	{
+		if(queue.dequeue(val))	printf("%d ", val);
+	}
+
+	printf("\n");
+
+	for(unsigned int i = 0; i < 10; ++i)
+	{
+		if(queue.dequeue(val))	printf("%d ", val);
+		else	printf("empty\n");
+	}
+
+	for(unsigned int i = 0; i < 20; ++i)
+	{
+		queue.enqueue(i);
+	}
+}
+
+void testRandomStack()
+{
+	StackArray<int> stack;
+	int tmp, r;
+	for(unsigned int i = 0; i < 10000; ++i)
+	{
+		r = rand();
+		if(r % 2)
+		{
+			stack.push(i);
+		}
+		else
+		{
+			stack.pop(tmp);
+		}
+	}
+}
+
+void testRandomQueue()
+{
+	QueueArray<int> queue;
+	int tmp, r;
+	for(unsigned int i = 0; i < 10000; ++i)
+	{
+		r = rand();
+		if(r % 2)
+		{
+			queue.enqueue(i);
+		}
+		else
+		{
+			queue.dequeue(tmp);
+		}
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	testStack();
+	testQueue();
+
+	testRandomStack();
+	testRandomQueue();
 
 	return 0;
 }
